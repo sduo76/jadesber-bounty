c3c8ad9a4ae3b84df68b89fb96e05226c96fb1d0
ana117 <63540951+ana117@users.noreply.github.com>
2023-08-29 06:21:43 +0700

    asd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [countdown, setCountdown] = useState(3);
+
+  useEffect(() => {
+    if (countdown > 0) {
+        setTimeout(() => {
+            setCountdown(countdown - 1);
+        }, 1000);
+    } else {
+        window.location.href = "https://www.google.com";
+    }
+  }, [countdown]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Redirecting in {countdown} seconds...
     </div>
   );
 }
